3d1cfba5231330e423de9e163fbc91154e0b458e
Matthias Blank <Matthias.Blank2@gmx.net>
2023-06-13 22:34:14 +0200

    Nächster Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FrontendAnnotationTool';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxAnnotateTextModule } from "ngx-annotate-text";
+import { FormsModule } from '@angular/forms';  // Import FormsModule
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatIconModule} from "@angular/material/icon";
+import { AnnotationPageComponent } from './annotation-page/annotation-page.component';
+import { ProjectListPageComponent } from './project-list-page/project-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnotationPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    ProjectListPageComponent,
+    BrowserAnimationsModule,
+    NgxAnnotateTextModule,
+    FormsModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
