9b22cdf0bfea3d28a8f3b171537f3283212f5d4e
Matthias Blank <Matthias.Blank2@gmx.net>
2023-07-13 20:17:01 +0200

    Annotation with Rest

--- src/app/annotation-page/annotation-page.component.ts
+++ src/app/annotation-page/annotation-page.component.ts
@@ -1,5 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {Annotation, NgxAnnotateTextComponent} from "ngx-annotate-text";
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
 @Component({
   selector: 'app-annotation-page',
   templateUrl: './annotation-page.component.html',
@@ -7,6 +9,14 @@ import {Annotation, NgxAnnotateTextComponent} from "ngx-annotate-text";
 })
 export class AnnotationPageComponent {
   @ViewChild('annotateText') ngxAnnotateText?: NgxAnnotateTextComponent;
+  id: string | null | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {
+  }
+
   text = 'Am 1. August sind wir in den Urlaub nach Barcelona, Spanien, gefahren. Unser Flug startete um 11:00 Uhr morgens. Wir hatten eine aufregende Reise vor uns und waren voller Vorfreude auf all die Abenteuer, die uns erwarten sollten.Als wir am Flughafen ankamen, war die Atmosphäre lebhaft und voller Aufregung. Menschen unterschiedlicher Nationalitäten eilten durch die Terminals, und das Summen der Gespräche und das Klappern der Rollkoffer erfüllten die Luft. Nachdem wir eingecheckt und unsere Bordkarten erhalten hatten, begaben wir uns zur Sicherheitskontrolle. Das Aufgeben unseres Gepäcks verlief reibungslos, und bald darauf waren wir bereit, das Flugzeug zu besteigen.'
   annotations: Annotation[] = [
     new Annotation(3, 11, 'Date', '#0d6efd'),
@@ -15,10 +25,10 @@ export class AnnotationPageComponent {
     new Annotation(77, 85, 'Time', '#6c757d'),
   ];
   annotationButtons = [
-    { label: 'Date', color: '#0d6efd' },
-    { label: 'Time', color: '#6c757d' },
-    { label: 'Country', color: '#198754' },
-    { label: 'City', color: '#dc3545' },
+    {label: 'Date', color: '#0d6efd'},
+    {label: 'Time', color: '#6c757d'},
+    {label: 'Country', color: '#198754'},
+    {label: 'City', color: '#dc3545'},
   ];
   events: string[] = [];
   newAnnotationColor = '#000000';
@@ -37,16 +47,25 @@ export class AnnotationPageComponent {
       return;
     }
     if (!this.annotationButtons.some(button => button.label === label)) {
-      this.annotationButtons.push({ label, color });
+      this.annotationButtons.push({label, color});
     }
     const annotation = new Annotation(selection.startIndex, selection.endIndex, label, color);
     this.annotations = this.annotations.concat(annotation);
     this.events.push(`Added '${annotation}'`);
   }
+
   onClickAnnotation(annotation: Annotation) {
     this.events.push(`Clicked on '${annotation}'`);
   }
+
   onRemoveAnnotation(annotation: Annotation): void {
     this.events.push(`Removed '${annotation}'`);
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.http.get(`http://localhost:8080/api/documents/${this.id}`).subscribe(data => {
+      // Handle data here
+    });
   }
 }
--- src/app/project-list-page/project-list-page.component.ts
+++ src/app/project-list-page/project-list-page.component.ts
@@ -8,34 +8,20 @@ import { FormGroup, Validators } from '@angular/forms';
 import {UploadService} from "../upload-service.service";
 import {ProjectService} from "../api-service.service";
 import {MainObject} from "../MainObject";
-
-export interface Element {
-  position: number;
-  name: string;
-  link: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  { position: 1, name: 'Hydrogen', link: 'https://example.com/hydrogen' },
-  { position: 2, name: 'Helium', link: 'https://example.com/helium' },
-  { position: 3, name: 'Lithium', link: 'https://example.com/lithium' },
-];
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-list-page',
   templateUrl: './project-list-page.component.html',
   styleUrls: ['./project-list-page.component.css'],
 })
-
 export class ProjectListPageComponent implements AfterViewInit {
   displayedColumns: string[] = ['position', 'name', 'link', 'export'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<MainObject>();
   uploadForm: FormGroup;
-  selectedFile: File | undefined;
   private selectedFormat: any;
-  projects: MainObject[] = [];
 
-  constructor(private projectService: ProjectService, private sidenavService: SidenavService, private _liveAnnouncer: LiveAnnouncer, private formBuilder: FormBuilder, private uploadService: UploadService) {
+  constructor(private projectService: ProjectService, private sidenavService: SidenavService, private _liveAnnouncer: LiveAnnouncer, private formBuilder: FormBuilder, private uploadService: UploadService, private router: Router) {
     this.uploadForm = this.formBuilder.group({
       projectName: ['', Validators.required]
     });
@@ -53,19 +39,20 @@ export class ProjectListPageComponent implements AfterViewInit {
       this._liveAnnouncer.announce('Sorting cleared');
     }
   }
-  onLinkClick(element: Element) {
+
+  onLinkClick(element: MainObject) {
     console.log(`Link clicked for element: ${element.name}`);
+    this.router.navigate(['/annotation', { id: element.guid }]);
   }
-  onExportClick(element: Element) {
+  onExportClick(element: MainObject) {
     console.log(`Export clicked for element: ${element.name}`);
   }
-
   onFormatChange(event: any, element: any) {
     this.selectedFormat = event.value;
   }
   ngOnInit(): void {
     this.projectService.getProjects().subscribe(data => {
-      this.projects = data;
+      this.dataSource.data = data;
     });
   }
 }
